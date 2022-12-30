66f1f2848073eb28843e80346f2ba833e13efaa0
unknown <1867792@students.wits.ac.za>
2022-12-30 08:00:03 +0200

    Kka

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(authRoute)
 
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
