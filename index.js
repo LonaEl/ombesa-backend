71a86daf8d404bec5e0004559e08101f40060aa6
unknown <1867792@students.wits.ac.za>
2022-12-30 06:32:42 +0200

    kia

--- index.js
+++ index.js
@@ -13,19 +13,23 @@ import authRoute from "./routes/auth.js";
 
   const app = express();
 
-  
+
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
- 
- 
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.get('/*', function(req, res) { res.sendFile('index.html');});
 
 app.use('/posts', postRoutes); 
 app.use(authRoute)
 
 
-app.get('/*', function(req, res) { res.sendFile('index.html');});
+
+
 app.get("/", (req, res, next) => {
   res.send("Api running");
 });
